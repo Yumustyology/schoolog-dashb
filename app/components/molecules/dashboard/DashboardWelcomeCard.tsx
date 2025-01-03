899e6dc495cae5267add295c7343552ade879314
Yusuf mustahan <yumustyology@gmail.com>
2025-01-04 00:28:14 +0100

    fix:all-modules

--- app/components/molecules/dashboard/DashboardWelcomeCard.tsx
+++ app/components/molecules/dashboard/DashboardWelcomeCard.tsx
@@ -18,10 +18,7 @@ const DashboardWelcomeCard = ({
       )}
     >
       {getRandomBinary() ? <Meteors number={20} /> : <Particles />}
-      {/* <Meteors number={20} /> <Particles /> */}
-      {/* <div className="w-[180px] h-[180px] tablet:w-[200px] tablet:h-[200px] rounded-full border-8 border-[#00B59566] absolute -top-[150px] right-[90px] --tablet:-right-[180px]-tablet:-top-[3px]"></div> */}
-      {/* <div className="w-[180px] h-[180px] tablet:w-[200px] tablet:h-[200px] rounded-full border-8 border-[#00B59566] absolute top-[150px] left-[40px]"></div> */}
-
+   
       <div className="w-[180px] h-[180px] tablet:w-[274px] rounded-full tablet:h-[200px]  bg-[#00B59566] absolute -left-[90px] -top-[40px] tablet:-left-[197px] tablet:-top-[100px]"></div>
       <div className="w-[180px] h-[180px] tablet:w-[274px] rounded-full tablet:h-[200px] bg-[#00B59566] absolute -right-[190px] top-[40px] tablet:top-[100px]"></div>
 
