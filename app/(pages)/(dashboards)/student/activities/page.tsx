7eea3d8b6dedf31ee2a947cdad6e127eec9d62b1
Yusuf mustahan <yumustyology@gmail.com>
2025-01-04 23:53:49 +0100

    fix:all-modules

--- app/(pages)/(dashboards)/student/activities/page.tsx
+++ app/(pages)/(dashboards)/student/activities/page.tsx
@@ -64,7 +64,7 @@ function page() {
         <Tabs value={activeActivitiestTab}>
           <div className="flex justify-between items-center">
             <div className="flex gap-6 ">
-              <div className="flex justify-start gap-6 bg-[#F7F7F7] border border-gray4 rounded-[100px] mb-6 p-2 h-[38px] w-[200px]">
+              <div className="flex justify-start gap-6 bg-[#F7F7F7] border border-gray4 rounded-[100px] mb-6 p-2 h-[38px] w-[200px] lgDesktop:w-auto">
                 <svg
                   width="18"
                   height="18"
