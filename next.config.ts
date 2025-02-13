15f0ae940b9e2103b86a3cd14d7685b0c2dd3f94
Ilyas Rufai <rufaiilyas@gmail.com>
2025-02-13 20:01:39 +0100

    fix: deploy issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Required for static export
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
