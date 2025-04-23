48669f95f4284f5113e19a86deeb5b94fab7315c
Yusuf mustahan <yumustyology@gmail.com>
2025-04-23 08:37:39 +0100

    feat:slg-theme

--- app/(pages)/(dashboards)/school/timetable/page.tsx
+++ app/(pages)/(dashboards)/school/timetable/page.tsx
@@ -1,25 +1,11 @@
 'use client';
 
-import { setTheme, themeState } from '@/app/lib/entities/theme.entity';
-import { themes } from '@/app/lib/themes/themeConfig';
 import React from 'react';
 
 const Page = () => {
-  const currentTheme = themeState.use();
   return (
     <div>
-      foo
-      <select
-        value={currentTheme}
-        onChange={(e) => setTheme(e.target.value as keyof typeof themes)}
-        className="border p-2 rounded"
-      >
-        {Object.keys(themes).map((key) => (
-          <option key={key} value={key}>
-            {key}
-          </option>
-        ))}
-      </select>
+    
     </div>
   );
 };
--- components/organisms/settings/EditProfile.tsx
+++ components/organisms/settings/EditProfile.tsx
@@ -6,8 +6,12 @@ import Button from '../../atoms/form/Button';
 import SelectComp from '../../atoms/form/Select';
 import { CountriesSelect } from '../../atoms/form/CountriesSelect';
 import { PhoneNumberInput } from '../../atoms/form/PhoneNumberInput';
+import { setTheme, themeState } from '@/app/lib/entities/theme.entity';
+import { themes } from '@/app/lib/themes/themeConfig';
 
 const EditProfile = () => {
+  const currentTheme = themeState.use();
+
   return (
     <div>
       <form>
@@ -145,6 +149,21 @@ const EditProfile = () => {
           </div>
         </div>
       </form>
+
+      <div>
+        foo
+        <select
+          value={currentTheme}
+          onChange={(e) => setTheme(e.target.value as keyof typeof themes)}
+          className="border p-2 rounded"
+        >
+          {Object.keys(themes).map((key) => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
